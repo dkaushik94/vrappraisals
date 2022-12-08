0ae55bfb2f7ee720fb69f2080014f802bbd887ee
Matthew Dickinson <matthew.dickinson@snapsheet.me>
2022-12-08 15:12:44 -0500

    Moved to auto and multi file uploads

--- src/S3DropZone.js
+++ src/S3DropZone.js
@@ -11,38 +11,86 @@ const fileTypes = ["JPG", "PNG", "GIF", "JPEG", "OBJ", "MTL"];
 
 const config = {
   bucketName: 'vrappraisals-demo-files',
+  dirName: 'incidents/29383493',
   region: 'us-east-2',
   accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
   secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
 }
 
-const S3DropZone = () =>{ 
-  const [file, setFile] = useState(null);
+const S3DropZone = () =>{
+  const [startedUpload, setStartedUpload] = useState(false);
+  const [objFile, setObjFile] = useState(null);
+  const [mtlFile, setMtlFile] = useState(null);
+  const [jpgFile, setJpgFile] = useState(null);
 
-  const handleChange = (file) => {
-    setFile(file);
+  const handleChange = (change) => {
+    console.log(change);
+    const files = change instanceof FileList ? Array.from(change) : [change];
+    console.log('files', files)
+    // setFile(file);
+    // files.push(file);
+    for (const file of files) {
+      uploadFileToS3(file).then(r => {
+        // const fileIdx = files.indexOf()
+      });
+
+      setFile(file);
+    }
   };
+  const setFile = file => {
+    const newFile = {name: file.name, uploadCompleted: file.uploadCompleted};
+    if (file.name.endsWith('.mtl')) {
+      setMtlFile(newFile);
+    } else if (file.name.endsWith('.obj')){
+      setObjFile(newFile);
+    } else {
+      setJpgFile(newFile);
+    }
+  }
 
-  const uploadFileToS3 = async () => {
+  const uploadFileToS3 = async (file) => {
     if(!file) {
       alert('No file Selected'); // Put a toast here instead later.
       return;
     }
+    setStartedUpload(true);
 
     const s3Client = new S3(config);
-    s3Client.uploadFile(file, file.name)
-    .then(data => alert('Your file has been successfully uploaded to:' + data.location))
-    .catch(err => console.error(err));
+    return s3Client.uploadFile(file, file.name)
+      .then(data => {
+        file.uploadCompleted = true;
+        setFile(file);
+        console.log('Your file has been successfully uploaded to:' + data.location)
+        return file;
+      })
+      .catch(err => {
+        file.uploadFailed = true;
+        setFile(file);
+        console.error(err)
+      });
   }
 
+  const renderFile = file => file && (
+      <div className={`item ${file.uploadCompleted ? 'completed': (file.uploadCompleted ? 'failed' : '')}`} >
+        <span>{file.name}</span>
+        <progress value={ file.uploadCompleted ? 100 : null}></progress>
+      </div>
+  )
   return (
     <div className='body'>
       <div className = 'dropZoneParent'>
-        <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+        <FileUploader handleChange={handleChange} multiple={true} name="file" types={fileTypes} />
       </div>
-      <div style = {{ margin: 10 }}>
-        <button onClick={uploadFileToS3}>Upload</button>
+      <div className="uploadList">
+        {renderFile(objFile)}
+        {renderFile(mtlFile)}
+        {renderFile(jpgFile)}
       </div>
+      {mtlFile?.uploadCompleted && objFile?.uploadCompleted && jpgFile?.uploadCompleted && <h3>You're done!</h3>}
+      {startedUpload && !(mtlFile?.uploadCompleted && objFile?.uploadCompleted && jpgFile?.uploadCompleted) && (<>
+        <h4>Uploading...</h4>
+        <p>Please be patient as this can take several minutes</p>
+      </>)}
     </div>
   );
 }
